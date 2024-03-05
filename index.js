86a71b17a0f651f726bb25ef611731e34c6f33c1
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-05 17:30:50 -0300

    Inject routers in app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const router = require("./routers/index");
+const router = require("./routers/index"); // Inject routers in app
 
 router(app);
 
