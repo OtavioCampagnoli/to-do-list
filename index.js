61e46f0b56047e85413e20ed5aa64916f3309ef2
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-03-05 21:07:36 -0300

    Inject infrastructure connection and tables.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const router = require("./routers/index"); // Inject routers in app
+const connection = require("./infrastructure/connection");
+const tables = require("./infrastructure/tables");
+
+tables.init(connection);
 
 router(app);
 
